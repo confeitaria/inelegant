01c9398613b91c77673de603db83b5281f7963bf
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-08-22 20:17:26 -0300

    #7 Documentation.

--- ugly/finder/finder.py
+++ ugly/finder/finder.py
@@ -19,4 +19,11 @@ class TestFinder(unittest.TestSuite):
                 pass
 
     def load_tests(self, loader, tests, pattern):
+        """
+        This method follows the ```load_tests() protocol`__. You can assign it
+        (when bound) to ``load_tests`` inside a module and then the
+        ``TestFinder`` will be the suite to be called.
+
+        __ https://docs.python.org/2/library/unittest.html#load-tests-protocol
+        """
         return self
