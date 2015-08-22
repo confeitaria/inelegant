d7ffcd340bb26f6b7056b4f3e1af36cd4778fd9f
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-08-22 18:27:08 -0300

    #4 Move module creation functions to their own module.

--- ugly/finder/test.py
+++ ugly/finder/test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from ugly.module import installed_module
+
 from ugly.finder import TestFinder
 
 class TestTestFinder(unittest.TestCase):
@@ -38,88 +40,3 @@ class TestTestFinder(unittest.TestCase):
             self.assertEquals(4, result.testsRun)
             self.assertEquals(1, len(result.failures))
             self.assertEquals(1, len(result.errors))
-
-import contextlib
-
-def create_module(name, code='', scope=None):
-    """
-    This function creates a module and adds it to the available ones::
-
-    >>> m = create_module('my_module')
-    >>> import my_module
-    >>> my_module == m
-    True
-
-    You can give the code of the module to it::
-
-    >>> m = create_module('with_code', code='x = 3')
-    >>> import with_code
-    >>> with_code.x
-    3
-
-    It also can receive a dictionary representing a previously set up scope
-    (i.e. containing values that will be set in the module)::
-
-    >>> m = create_module('with_scope', scope={'y': 32})
-    >>> import with_scope
-    >>> with_scope.y
-    32
-
-    It is possible to give both arguments as well and the code will work over
-    the scope::
-
-    >>> m = create_module('intricate', code='z = z+1', scope={'z': 4})
-    >>> import intricate
-    >>> intricate.z
-    5
-    """
-    import imp
-    import sys
-    import inspect
-
-    scope = scope if scope is not None else {}
-    module = imp.new_module(name)
-
-    exec(code, scope)
-
-    module_objs = [
-        obj for n, obj in scope.items()
-         if (
-            not n.startswith('__') and hasattr(obj, '__module__') and
-            (name not in locals()) and (name not in globals())
-        )
-    ]
-
-    for obj in module_objs:
-        obj.__module__ = name
-
-    module.__dict__.update(scope)
-    sys.modules[name] = module
-
-    return module
-
-@contextlib.contextmanager
-def installed_module(name, code='', scope=None):
-    """
-    This is a context manager to have a module created during a context::
-
-    >>> with installed_module('a', code='x=3', scope={'y': 4}) as m:
-    ...     import a
-    ...     m == a
-    ...     a.x
-    ...     a.y
-    True
-    3
-    4
-
-    On the context exit the module will be removed from ``sys.modules``::
-
-    >>> import a
-    Traceback (most recent call last):
-      ...
-    ImportError: No module named a
-    """
-    import sys
-
-    yield create_module(name, code, scope)
-    del sys.modules[name]
