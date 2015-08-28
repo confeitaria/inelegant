857521801e27bdf9f7e0c6807997d541932b856a
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-08-28 04:08:39 -0300

    #15 Use smaller waiting times.

--- ugly/net/test.py
+++ ugly/net/test.py
@@ -20,20 +20,19 @@ class TestServer(unittest.TestCase):
                 address='localhost', port=9000, message='Server is up'
             )
             server.handle_request()
+            server.socket.close()
             server.shutdown()
 
         process = multiprocessing.Process(target=serve)
         process.daemon = True
         process.start()
-        time.sleep(0.01)
+        time.sleep(0.003)
 
         with contextlib.closing(socket.socket()) as s:
             s.connect(('localhost', 9000))
             msg = s.recv(len('Server is up'))
 
             self.assertEquals('Server is up', msg)
-
-        time.sleep(0.001)
 
 from ugly.finder import TestFinder
 
