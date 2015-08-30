d5548b694063b8ec55fc5fb20313e2796c946d4c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-08-30 00:06:36 -0300

    #15 Postpone socket creation.
    For our tests, it is better to have th port free until the server effectively
    starts to listen. So, we are overriding some methods so the socket is only
    created and only connects to the port when the server starts to listen.

--- ugly/net/net.py
+++ ugly/net/net.py
@@ -26,10 +26,11 @@ class Server(SocketServer.TCPServer):
 
     ::
 
-    >>> import multiprocessing
+    >>> import multiprocessing, time
     >>> process = multiprocessing.Process(target=serve)
     >>> process.daemon = True
     >>> process.start()
+    >>> time.sleep(0.003)
 
     ...we can get the answer::
 
@@ -49,13 +50,20 @@ class Server(SocketServer.TCPServer):
         self.port = port
         self.message = message
         self.timeout = timeout
-        
-        SocketServer.TCPServer.__init__(
-            self, (self.address, self.port), ServerHandler
-        )
+
+    def handle_request(self):
+        self._lazy_init()
+
+        return SocketServer.TCPServer.handle_request(self)
+
+    def serve_forever(self, poll_interval=0.001):
+        self._lazy_init()
+
+        SocketServer.TCPServer.serve_forever(self, poll_interval)
 
     def __enter__(self):
         self.thread = threading.Thread(target=self._start)
+        self.thread.daemon = False
         self.thread.start()
         time.sleep(0.01)
         return self
@@ -67,6 +75,12 @@ class Server(SocketServer.TCPServer):
 
     def _start(self):
         self.serve_forever()
+
+    def _lazy_init(self):
+        if not hasattr(self, 'socket'):
+            SocketServer.TCPServer.__init__(
+                self, (self.address, self.port), ServerHandler
+            )
 
 class ServerHandler(SocketServer.BaseRequestHandler):
 
