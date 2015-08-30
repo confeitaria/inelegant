10a8b9d2875ac9079ce398101f018c457a4a7236
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-08-30 00:37:37 -0300

    Call tests from ugly.test

--- ugly/test.py
+++ ugly/test.py
@@ -1,6 +1,16 @@
+import unittest
+
 import ugly.finder.test
 import ugly.module.test
+import ugly.net.test
 
 from ugly.finder import TestFinder
 
-load_tests = TestFinder(ugly.finder.test, ugly.module.test).load_tests
+load_tests = TestFinder(
+    ugly.finder.test,
+    ugly.module.test,
+    ugly.net.test
+).load_tests
+
+if __name__ == "__main__":
+    unittest.main()
