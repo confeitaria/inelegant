97ed8994511b261ee0570de553dd9cf9926a3ea8
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-09-05 17:35:49 -0300

    #14 Test to ensure wait_server_up() does not acquire socket.

--- ugly/net/test.py
+++ ugly/net/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 import multiprocessing
+import threading
 import socket
 import contextlib
 import time
@@ -106,6 +107,53 @@ class TestWaiters(unittest.TestCase):
             wait_server_up('localhost', 9000, timeout=0.001)
 
             self.assertTrue(time.time() - start > delay)
+
+    def test_wait_port_up_does_not_acquire_port(self):
+        """
+        ``wait_server_up()`` cannot impede other processes of capturing the
+        port.
+        """
+        delay = 0.01
+        start = time.time()
+
+        def serve(condition, queue):
+            try:
+                time.sleep(delay)
+
+                server = Server(message='Server is up')
+
+                thread = threading.Thread(target=server.serve_forever)
+                thread.start()
+
+                condition.acquire()
+                condition.wait()
+
+                server.shutdown()
+                thread.join()
+            except Exception as e:
+                queue.put(e)
+
+        condition = multiprocessing.Condition()
+        queue = multiprocessing.Queue()
+        process = multiprocessing.Process(target=serve, args=(condition, queue))
+        process.start()
+
+        wait_server_up('localhost', 9000, timeout=delay*2)
+
+        with contextlib.closing(get_socket()) as s:
+            s.connect(('localhost', 9000))
+            msg = s.recv(len('Server is up'))
+
+            self.assertEquals('Server is up', msg)
+
+        condition.acquire()
+        condition.notify_all()
+        condition.release()
+
+        process.join()
+
+        if not queue.empty():
+            raise queue.get()
 
 from ugly.finder import TestFinder
 
