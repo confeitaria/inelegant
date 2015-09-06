3e0feae3ab6c854510297f4631dd7e6ca2c2690c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-09-05 23:33:55 -0300

    #22 Save the raised exception.
    How we will use it in practice is yet to be defined. In general, a failure in
    the spawned process results from a failure in the original process. This means
    that an exception was already raised by the original process and, if it is not
    treated, the original process finishes before any treatment to be done at
    pc.exceptions.
    
    Nonetheless, this is a very important feature to have. It is dastardly annoying
    to not have ANY way to check for exceptions from the spawned process.

--- ugly/process/process.py
+++ ugly/process/process.py
@@ -4,7 +4,11 @@ class ProcessContext(object):
 
     def __init__(self, target, args=(), timeout=1):
         self.timeout = timeout
-        self.process = multiprocessing.Process(target=target, args=args)
+        self.exceptions = []
+        self.exceptions_queue = multiprocessing.Queue()
+        self.process = multiprocessing.Process(
+            target=self.queue_exception(target), args=args
+        )
 
     def __enter__(self):
         self.process.start()
@@ -15,4 +19,14 @@ class ProcessContext(object):
             self.process.terminate()
 
         self.process.join(self.timeout)
+        while not self.exceptions_queue.empty():
+            self.exceptions.append(self.exceptions_queue.get())
 
+    def queue_exception(self, target):
+        def f(*args, **kwargs):
+            try:
+                target(*args, **kwargs)
+            except Exception as e:
+                self.exceptions_queue.put(e)
+
+        return f
