1ac8c27aacc7c5d0da8a461d573fc4c8eba5e461
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-09-08 01:14:57 -0300

    #23 Test for "conversation" with process.
    Communicating with the process from the context is a common need. Here we show
    how one is expected to do it in a more succinct (although not necessarily
    simpler) way.
    
    If the function given to the process context is a generator function, then the
    generator can expect values from the original process. Also, the original
    process will have the values yielded by the generator.

--- ugly/process/test.py
+++ ugly/process/test.py
@@ -69,6 +69,28 @@ class TestProcessContext(unittest.TestCase):
         self.assertIsInstance(pc.exceptions[0], AssertionError)
         self.assertEquals('Actually, it is expected', pc.exceptions[0].args[0])
 
+    def test_send_receive_data(self):
+        """
+        Blocks managed by ``ProcessContext`` should ensure a paralel process
+        starts and, after the execution of the block, the process should have
+        been finished.
+        """
+        def serve(value):
+            value = yield value
+            yield value
+
+        with ProcessContext(target=serve, args=(1,)) as pc:
+            value = pc.get()
+            self.assertEquals(1, value)
+
+            value = pc.send(2)
+            value = pc.get()
+            self.assertEquals(2, value)
+
+            pc.go()
+
+        if pc.exceptions:
+            raise pc.exceptions[0]
 
 from ugly.finder import TestFinder
 
