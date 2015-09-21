f98bf15e648f74ffcd925a76ed920f4937a73c33
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-09-20 22:32:56 -0300

    #29 Refactor code to make less method calls.

--- ugly/finder/finder.py
+++ ugly/finder/finder.py
@@ -12,10 +12,13 @@ class TestFinder(unittest.TestSuite):
     """
     def __init__(self, *testables):
         unittest.TestSuite.__init__(self)
+        self.doctest_finder = doctest.DocTestFinder(exclude_empty=False)
 
         for testable in testables:
+            doctestable = None
+            module = None
             if isinstance(testable, file):
-                self.add_doctests(testable.name)
+                doctestable = testable.name
             elif isinstance(testable, basestring):
                 if testable == '.':
                     caller = sys._getframe(1)
@@ -25,26 +28,35 @@ class TestFinder(unittest.TestSuite):
 
                 try:
                     module = importlib.import_module(name)
-                    self.add_tests_from_module(module)
+                    doctestable = module
                 except ImportError:
-                    self.add_doctests(name)
-            else:
-                self.add_tests_from_module(testable)
+                    doctestable = name
+            elif inspect.ismodule(testable):
+                module = testable
+                doctestable = module
+
+            if module is not None:
+                self.add_tests_from_module(module)
+            if doctestable is not None:
+                self.add_doctests(doctestable)
 
     def add_doctests(self, file_name):
-        module_relative = not file_name.startswith(os.sep)
-        self.addTest(
-            doctest.DocFileSuite(file_name, module_relative=module_relative)
-        )
+        if inspect.ismodule(file_name):
+            suite = doctest.DocTestSuite(
+                file_name, test_finder=self.doctest_finder
+            )
+        else:
+            module_relative = not file_name.startswith(os.sep)
+            suite = doctest.DocFileSuite(
+                file_name, module_relative=module_relative
+            )
+
+        self.addTest(suite)
 
     def add_tests_from_module(self, module):
         self.addTest(
             unittest.defaultTestLoader.loadTestsFromModule(module)
         )
-        try:
-            self.addTest(doctest.DocTestSuite(module))
-        except ValueError:
-            pass
 
     def load_tests(self, loader, tests, pattern):
         """
