86cceaf9bcc193d96c4bc6cfd1a64fdf66bc40dd
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-10-11 21:06:07 -0300

    #9 Run README file with the tests.

--- ugly/test.py
+++ ugly/test.py
@@ -1,4 +1,5 @@
 import unittest
+import os, os.path
 
 import ugly.finder.test
 import ugly.module.test
@@ -7,7 +8,14 @@ import ugly.process.test
 
 from ugly.finder import TestFinder
 
+readme_path = os.path.join(
+    os.path.dirname(__file__),
+    os.pardir,
+    'readme.rst'
+)
+
 load_tests = TestFinder(
+    readme_path,
     ugly.finder.test,
     ugly.module.test,
     ugly.net.test,
