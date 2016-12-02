3d7d475735be68364bc58d0343dc5840c739df1f
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-12-02 12:57:36 -0300

    New release! 0.1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inelegant',
-    version="0.0.9",
+    version="0.1.0",
     author='Adam Victor Brandizzi',
     author_email='adam@brandizzi.com.br',
     description='Inelegant, a directory of weird helpers for tests.',
